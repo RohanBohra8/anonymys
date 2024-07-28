dec5ac6c746e37ac614a1118ffbafc575df0f2d0
Rohan Bohra <rohanbohra8@gmail.com>
2024-07-28 08:35:20 +0530

    some changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import AuthProvider from '../context/AuthProvider';
+//import AuthProvider from '../context/AuthProvider';
 import { Toaster } from '@/components/ui/toaster';
+import AuthProvider from '@/context/authProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
